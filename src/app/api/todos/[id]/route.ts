4c15913b237938c338a8aaad4d8531d2dd8a949b
joemar25 <joemar.cardino000@gmail.com>
2025-01-16 09:05:08 +0800

    next-js 15 convention

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,15 +2,22 @@ import { auth } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
+type Params = {
+    params: {
+        id: string;
+    };
+};
+
 export async function PATCH(
     request: NextRequest,
-    { params }: { params: { id: string } }
+    context: Params
 ) {
     const session = await auth()
     if (!session?.user) {
         return new NextResponse('Unauthorized', { status: 401 })
     }
 
+    const params = await context.params
     const json = await request.json()
     const todo = await prisma.todo.updateMany({
         where: {
@@ -25,13 +32,14 @@ export async function PATCH(
 
 export async function DELETE(
     request: NextRequest,
-    { params }: { params: { id: string } }
+    context: Params
 ) {
     const session = await auth()
     if (!session?.user) {
         return new NextResponse('Unauthorized', { status: 401 })
     }
 
+    const params = await context.params
     await prisma.todo.deleteMany({
         where: {
             id: params.id,
